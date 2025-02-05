1a8a70432bf765558c8513591ad9109a04aecdae
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-02-05 09:32:44 +0545

    adding jwt

--- PORTAL/settings.py
+++ PORTAL/settings.py
@@ -58,6 +58,10 @@ ROOT_URLCONF = 'PORTAL.urls'
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    )
 }
 
 
--- api/urls.py
+++ api/urls.py
@@ -48,3 +48,13 @@ urlpatterns = [
         name="comment-api",
     ),
 ]
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+
+    path('/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
